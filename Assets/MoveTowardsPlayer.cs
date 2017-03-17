2a83671b03f49bc7ef12f057807afce238701dfa
Jonathan Livingstone <seagull127@ymail.com>
2017-03-17 17:09:35 +0000

    fjkl

---
+++ Assets/MoveTowardsPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsPlayer : MonoBehaviour {
+    public GameObject target;
+    public float distance;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Vector3.Distance(target.transform.position, transform.position) < distance) {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+
+            Vector3 targetDir = target.transform.position - transform.position;    
+            float step = 1 * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+	}
+}
--- Assets/UnityVS/Player.cs
+++ Assets/UnityVS/Player.cs
@@ -4,11 +4,21 @@ using System.Collections;
 public class Player : MonoBehaviour {
     public float movementSpeed = 500;
     public float turningSpeed = 120;
+    public GameObject back;
+    public GameObject text;
+    public GameObject line;
     public Rigidbody rb;
     public bool onGround;
+    Vector3 checkpoint;
+
+    float timer = 0;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+
+        back.SetActive(false);
+        text.SetActive(false);
+        line.SetActive(false);
     }
 
     void OnCollisionStay (Collision collisionInfo) {onGround = true;}
@@ -17,6 +27,18 @@ public class Player : MonoBehaviour {
     void FixedUpdate() {
         if ((onGround == true) && (Input.GetButtonDown("Jump"))) {
             rb.velocity = new Vector3(0, 15, 0);
+        }
+    }
+
+     void OnTriggerEnter(Collider collider) {
+        if (collider.name.Equals("Trigger"))
+        { 
+            back.SetActive(true);
+            text.SetActive(true);
+            line.SetActive(true);
+            timer = 120;
+        } else if (collider.name.Equals("BoundingBox")) {
+            transform.position = checkpoint;      
         }
     }
 
@@ -30,6 +52,18 @@ public class Player : MonoBehaviour {
         if(Input.GetKey("j")) {
             transform.Translate(0, 1, 0);
             rb.velocity = new Vector3(0, 0, 0);
+        }
+            
+        if (onGround) {
+            checkpoint = transform.position;
+        }
+        
+        if(timer > 0) {
+            timer -= 1;
+        } else {
+            back.SetActive(false);
+            text.SetActive(false);
+            line.SetActive(false);
         }
     }
 }
